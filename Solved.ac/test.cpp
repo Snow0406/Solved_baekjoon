e447ebb389468a1e22292e768790f9b892ba8026
Snow <87596507+Snow0406@users.noreply.github.com>
2024-09-10 19:31:17 +0900

    ud

--- Solved.ac/test.cpp
+++ Solved.ac/test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+<<<<<<< Updated upstream
 #include <algorithm>
 
 using namespace std;
@@ -34,6 +35,79 @@ int main() {
     }
     
     cout << max_length << endl;
+=======
+#include <queue>
+
+using namespace std;
+
+struct Train {
+    int to;
+    long long start, end;
+    Train(int t, long long s, long long e) : to(t), start(s), end(e) {}
+};
+
+struct State {
+    int station;
+    long long time;
+    State(int s, long long t) : station(s), time(t) {}
+    bool operator>(const State s) const {
+        return time > s.time;
+    }
+};
+
+const long long MAX = 1e18;
+vector<vector<Train>> graph;
+int N, M, K, Q;
+
+long long train_sim(int startStation, long long startTime) {
+    vector<long long> list(N + 1, MAX);
+    priority_queue<State, vector<State>, greater<>> pq;
+    
+    list[startStation] = startTime;
+    pq.emplace(startStation, startTime);
+    
+    while (!pq.empty()) {
+        const State current = pq.top();
+        pq.pop();
+        
+        if (current.station == K) return current.time;
+        if (current.time > list[current.station]) continue;
+        
+        for (const Train& train : graph[current.station]) {
+            if (current.time <= train.start && train.end < list[train.to]) {
+                list[train.to] = train.end;
+                pq.emplace(train.to, train.end);
+            }
+        }
+    }
+    
+    return -1;
+}
+
+int main() {
+    //외부 소스 : https://kimcoder.tistory.com/49
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin >> N >> M >> K >> Q;
+    graph.resize(N + 1);
+    
+    for (int i = 0; i < M; i++) {
+        int s, d;
+        long long ts, td;
+        cin >> s >> ts >> d >> td;
+        graph[s].emplace_back(d, ts, td);
+    }
+    
+    for (int i = 0; i < Q; i++) {
+        int s;
+        long long t;
+        cin >> s >> t;
+        const long long result = train_sim(s, t);
+        cout << result << '\n';
+    }
+>>>>>>> Stashed changes
     
     return 0;
 }
