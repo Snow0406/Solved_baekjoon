8f05e3e73eddc2b32b8975b6c23d7765d447e538
Snow <87596507+Snow0406@users.noreply.github.com>
2024-09-21 03:57:55 +0900

    add: 2588

---
+++ BaekJoon/Bronze/2588.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int A, B;
+   cin >> A >> B;
+
+    int a = A * (B % 10);
+    int b = A * ((B % 100) / 10);
+    int c = A * ((B % 1000) / 100);
+    
+    cout << a << "\n";
+    cout << b << "\n";
+    cout << c << "\n";
+    cout << a + (b*10) + (c*100) << "\n";
+    
+    return 0;
+}
