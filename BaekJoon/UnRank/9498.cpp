2bd7103dabece9ba4eba88f116bcd287538e5f8a
Snow <87596507+Snow0406@users.noreply.github.com>
2024-09-26 01:39:55 +0900

    9498.cpp

---
+++ BaekJoon/UnRank/9498.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int score;
+    cin >> score;
+
+    score /= 10;
+    switch (score)
+    {
+    case 10:
+        cout << "A" << "\n";
+        break;
+    case 9:
+        cout << "A" << "\n";
+        break;
+    case 8:
+        cout << "B" << "\n";
+        break;
+    case 7:
+        cout << "C" << "\n";
+        break;
+    case 6:
+        cout << "D" << "\n";
+        break;
+    default:
+        cout << "F" << "\n";
+        break;
+    }
+
+    return 0;
+}
